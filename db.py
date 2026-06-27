c4e5368a858f6192682a70ef1c57efc2c5c26bc2
Blue Tech Consulting <blue.techconsulting2025@gmail.com>
2026-06-27 20:36:20 -0300

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def get_conn():
     return psycopg2.connect(
-        host=st.secrets["SUPABASE_HOST"],
+        host= "db.dvetmverdsdbegvmrbty.supabase.co",
         database=st.secrets["SUPABASE_DB"],
         user=st.secrets["SUPABASE_USER"],
         password=st.secrets["SUPABASE_PASS"],
