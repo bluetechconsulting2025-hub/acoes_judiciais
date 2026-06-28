1d4d1ca087dca0188910ee725515798fb89ce7c0
Blue Tech Consulting <blue.techconsulting2025@gmail.com>
2026-06-28 19:50:41 -0300

    Update Ações_Judiciais.py

--- pages/Ações_Judiciais.py
+++ pages/Ações_Judiciais.py
@@ -96,7 +96,6 @@ def criar_appointment_wms(cpf_limpo, processo, data_saida, numero_pedido):
 
     payload = {
         "appointmentkey": processo,
-        "storerkey": cpf_limpo,
         "type": "1",
         "gmtstartdateandtime": f"{data_saida.strftime('%Y-%m-%d')}T00:00:00-03:00",
         "gmtenddateandtime": f"{data_saida.strftime('%Y-%m-%d')}T01:00:00-03:00",
@@ -104,6 +103,7 @@ def criar_appointment_wms(cpf_limpo, processo, data_saida, numero_pedido):
             {
                 "appointmentkey": processo,
                 "sourcekey": numero_pedido,
+                "storerkey": cpf_limpo,
                 "sourcetype": "1"
             }
         ]
